6aaf13d7e16eae4540af7f3b293f369ea90042b9
Zid <rashidvisda@gmail.com>
2026-09-11 19:26:59 +0800

    build: update nextjs ver

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,17 @@
-import { Button } from "@/components/ui/button"
+import { createClient } from "@/utils/supabase/server"
+import { cookies } from "next/headers"
 
-export default function Page() {
+export default async function Page() {
+  const cookieStore = await cookies()
+  const supabase = createClient(cookieStore)
+
+  const { data: todos } = await supabase.from("todos").select()
+
   return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
-        </div>
-        <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
-        </div>
-      </div>
-    </div>
+    <ul>
+      {todos?.map((todo) => (
+        <li key={todo.id}>{todo.name}</li>
+      ))}
+    </ul>
   )
 }
